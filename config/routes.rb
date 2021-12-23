19db8ba7f86b430b39fac77a1f655a8100bf99ad
gormlessfie <gmlessfie@gmail.com>
2021-12-23 15:09:10 -0800

    Configure routes to user/registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions',
+                                    registrations: 'users/registrations' }
 
   resources :posts
 end
