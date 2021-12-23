39326f3673d8fd87f1f09308148e21b19ccd8863
gormlessfie <gmlessfie@gmail.com>
2021-12-23 14:03:49 -0800

    Add bulma styles to login and signup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,26 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:success] = 'Post successful.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Post unsuccessful.'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
