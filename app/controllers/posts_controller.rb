2b07c8313b0121122283d0c72e760392d86e9dc3
gormlessfie <gmlessfie@gmail.com>
2021-12-23 16:47:12 -0800

    Move form into partial and create edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to user_post_path(@post.user.id, @post), notice:'Post updated successfully'
+    else
+      flash.now[:error] = 'Something went wrong'
+      render :edit
+    end
+  end
+
   private
 
   def post_params
